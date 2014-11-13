05386e3cb6f1011dd058e5bf4a17c08474c00b15
Hayk Saakian <fakest.id.ever@gmail.com>
2014-11-13 11:54:27 -0800

    get jquery

--- strims.php
+++ strims.php
@@ -26,6 +26,7 @@ require_once 'session.php';
   </head>
 
   <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <?php 
     include ('navbar.php');
     include('strims_content.php');
@@ -34,7 +35,6 @@ require_once 'session.php';
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="../../dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
